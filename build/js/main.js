61850d21b1e07bcd0f1779f5cdb8e5b7180135af
Ishimwe Eric <ishimwee299@gmail.com>
2025-06-20 03:21:22 +0200

    Added a mobile toggle

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+// Toggle menu
+const initApp = ()=>{
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    // console.log(hamburgerBtn);
+    // console.log(mobileMenu);
+
+    const toggleMenu = ()=>{
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
